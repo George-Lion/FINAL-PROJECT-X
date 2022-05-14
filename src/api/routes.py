46ad8f071b565bf48bc18cb649243ac7e5f48aa5
Dans182 <dgaiteiro@gmail.com>
2022-05-14 21:02:51 +0000

    v1

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,18 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/login", methods=["POST"])
+def login_user():
+    body_email = request.json.get("email")
+    body_password = request.json.get("password")
+    if body_email and body_password:
+        user = User.query.filter_by(email=body_email).filter_by(
+            password=body_password).first()
+        if user:
+            token = create_access_token(identity=user.id)
+            return jsonify({"logged": True, "token": token, "user": user.serialize()}), 200
+        else:
+            return jsonify({"logged": False, "msg": "Bad info"}), 400
+    else:
+        return jsonify({"logged": False, "msg": "Missing info"}), 400
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,12 +1,34 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
+  return {
+    store: {
+      user: null,
+    },
+    actions: {
+      setStoreUser: (user) => {
+        setStore({ user: user });
+      },
 
-		},
-		actions: {
-
-		}
-	};
+      getUser: async () => {
+        let token = localStorage.getItem("token");
+        if (token != null && token != undefined) {
+          const response = await fetch(
+            "https://3001-georgelion-finalproject-d16qehmb8rn.ws-eu45.gitpod.io/api/user",
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+          const data = await response.json();
+          getActions().setStoreUser(data.user);
+        } else {
+          getActions().setStoreUser(null);
+        }
+      },
+    },
+  };
 };
 
 export default getState;
