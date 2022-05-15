79a2e435a7c7aa5cfa61f6709711a16ad8c7e9f2
Lvsyan <Lvsyan96@gmail.com>
2022-05-15 08:54:42 +0000

    Layouts

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,11 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
+import { Perfil } from "./pages/perfil";
+import { Private } from "./pages/private";
+import { Ruta } from "./pages/ruta";
+import { Rutas } from "./pages/rutas";
+import { Ayuda } from "./pages/ayuda";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -12,34 +17,49 @@ import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/login">
-							<Login />
-						</Route>
-						<Route exact path="/register">
-							<Register />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/perfil">
+              <Perfil />
+            </Route>
+            <Route exact path="/private">
+              <Private />
+            </Route>
+            <Route exact path="/ruta">
+              <Ruta />
+            </Route>
+            <Route exact path="/rutas">
+              <Rutas />
+            </Route>
+            <Route exact path="/ayuda">
+              <Ayuda />
+            </Route>
+            <Route>
+              <h1>Not found!</h1>
+            </Route>
+          </Switch>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
