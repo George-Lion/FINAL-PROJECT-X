c25bd6f250613f6f16d51469097a2c65fe55e1df
Dans182 <dgaiteiro@gmail.com>
2022-05-17 17:50:59 +0000

    layout listo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">HOME</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/login">
-						<button className="btn btn-primary">LOGIN</button>
-					</Link>
-					<Link to="/register">
-						<button className="btn btn-primary">SIGN UP</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      {store.logged != null ? (
+        <div className="container">
+          <Link to="/">
+            <span className="navbar-brand mb-0 h1">HOME</span>
+          </Link>
+          <div className="ml-auto">
+            {store.logged == true ? (
+              <Link
+                to="/"
+                onClick={() => {
+                  actions.logout();
+                }}
+              >
+                <button className="btn btn-primary">Logout</button>
+              </Link>
+            ) : (
+              <>
+                <Link to="/login">
+                  <button className="btn btn-primary">LOGIN</button>
+                </Link>
+                <Link to="/register">
+                  <button className="btn btn-primary">SIGN UP</button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      ) : null}
+    </nav>
+  );
 };
