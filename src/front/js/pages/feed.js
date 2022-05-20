4e57ef0791af21b9e2042f9b7509167b347da399
Dans182 <dgaiteiro@gmail.com>
2022-05-20 17:32:36 +0000

    v1

--- src/front/js/pages/feed.js
+++ src/front/js/pages/feed.js
@@ -4,29 +4,43 @@ import "../../styles/feed.css";
 export const Feed = () => {
   return (
     <>
-      <div className="container">
-        <div className="my-4">
-          <div className="justify-content-center">
-            <div className="card">
+      <div className="container position-relative">
+        <div className="my-4 justify-content-center card ">
+          <div className="d-flex me-5 position-absolute top-0 end-0 mt-3">
+            <span>
+              <i class="fas fa-map-marker"></i> Islas Malvinas
+            </span>
+          </div>
+          <img
+            className="card-img-top"
+            src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=800"
+            alt="Banner"
+          />
+          <div className="card-body little-profile">
+            <div className="position-absolute top-50 start-0 translate-middle-y pro-img">
               <img
-                className="card-img-top"
-                src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=800"
-                alt="Banner"
+                className="ms-3"
+                src="https://i.imgur.com/8RKXAIV.jpg"
+                alt="user"
               />
-              <div className="card-body little-profile text-center">
-                <div className="pro-img">
-                  <img src="https://i.imgur.com/8RKXAIV.jpg" alt="user" />
-                </div>
-                <h3 className="m-b-0">Brad Macullam</h3>
-                <div className="d-flex justify-content-around">
-                  <span className="">Web Designer &amp; Developer</span>
-                  <span className="">
-                    <i class="fa-solid fa-clock"></i>Web Designer &amp;
-                    Developer
-                  </span>
-                  <span className="">Web Designer &amp; Developer</span>
-                  <span className="">Web Designer &amp; Developer</span>
-                </div>
+            </div>
+            <div className="container">
+              <div className="position-absolute bottom-0 start-50 translate-middle-x mb-5">
+                <h3 className="">Brad Macullam</h3>
+              </div>
+
+              <div className="d-flex justify-content-around">
+                <span className="">Web Designer &amp; Developer</span>
+                <span className="">
+                  <i className="fas fa-clock"></i> August 4 - August 10 &amp;
+                  Developer
+                </span>
+                <span className="">
+                  <i class="fas fa-user-friends"></i> 2 - 6{" "}
+                </span>
+                <span className="">
+                  <i class="fas fa-heart"></i> 76{" "}
+                </span>
               </div>
             </div>
           </div>
