4e19f6339ee391520380422c1ca2b0e9c8e12d57
Dans182 <dgaiteiro@gmail.com>
2022-05-20 16:14:33 +0000

    v1

--- src/front/js/pages/feed.js
+++ src/front/js/pages/feed.js
@@ -5,7 +5,7 @@ export const Feed = () => {
   return (
     <>
       <div className="container">
-        <div className="mt-4">
+        <div className="my-4">
           <div className="justify-content-center">
             <div className="card">
               <img
@@ -18,30 +18,16 @@ export const Feed = () => {
                   <img src="https://i.imgur.com/8RKXAIV.jpg" alt="user" />
                 </div>
                 <h3 className="m-b-0">Brad Macullam</h3>
-                <p>Web Designer &amp; Developer</p>{" "}
-                <a
-                  href="javascript:void(0)"
-                  className="m-t-10 waves-effect waves-dark btn btn-primary btn-md btn-rounded"
-                  data-abc="true"
-                >
-                  Follow
-                </a>
-              </div>
-              <div className="row text-center m-t-20">
-                <div className="col-lg-4 col-md-4 m-t-20">
-                  <h3 className="m-b-0 font-light">10434</h3>
-                  <small>Articles</small>
-                </div>
-                <div className="col-lg-4 col-md-4 m-t-20">
-                  <h3 className="m-b-0 font-light">434K</h3>
-                  <small>Followers</small>
-                </div>
-                <div className="col-lg-4 col-md-4 m-t-20">
-                  <h3 className="m-b-0 font-light">5454</h3>
-                  <small>Following</small>
+                <div className="d-flex justify-content-around">
+                  <span className="">Web Designer &amp; Developer</span>
+                  <span className="">
+                    <i class="fa-solid fa-clock"></i>Web Designer &amp;
+                    Developer
+                  </span>
+                  <span className="">Web Designer &amp; Developer</span>
+                  <span className="">Web Designer &amp; Developer</span>
                 </div>
               </div>
-              {/* </div> */}
             </div>
           </div>
         </div>
