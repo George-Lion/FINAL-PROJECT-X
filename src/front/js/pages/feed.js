e04366b0170ba1aa36c4a49b754508fdb6860e52
Dans182 <dgaiteiro@gmail.com>
2022-05-21 09:14:19 +0000

    v3

--- src/front/js/pages/feed.js
+++ src/front/js/pages/feed.js
@@ -56,6 +56,42 @@ export const Feed = () => {
 
   return (
     <>
+      <div className="container-fluid p-3">
+        <div className="row">
+          <div className="col-sm-8">
+            <div className="">
+              <div className="row">
+                <div className="col">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
+                  </svg>
+                  <label
+                    className="me-sm-2"
+                    htmlFor="inlineFormCustomSelect"
+                  ></label>
+                  <select
+                    className="bg-white me-sm-2"
+                    id="inlineFormCustomSelect"
+                  >
+                    <option value="0">London</option>
+                    <option value="1">Dubai</option>
+                    <option value="2">Cairo</option>
+                    <option value="3">New York</option>
+                  </select>
+                </div>
+                <div className="col-xs-5" placeholder="Search">
+                  <input></input>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container ">
         {trips.map((e) => {
           return (
