ecfe36a90a68dc143faaee6b8a9fded2008126e4
Dans182 <dgaiteiro@gmail.com>
2022-05-23 17:15:48 +0000

    database done

--- src/api/models.py
+++ src/api/models.py
@@ -28,11 +28,11 @@ class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id_of_trip_creator = db.Column(db.Integer, db.ForeignKey("user.id"))
     destination = db.Column(db.String(120), unique=False, nullable=False)
-    date_of_the_trip = db.Column(db.Integer, unique=False, nullable=False)
+    date_of_the_trip = db.Column(db.Date(), unique=False, nullable=False)
     people = db.Column(db.Integer, unique=False, nullable=False)
     transport = db.Column(db.String(120), unique=False, nullable=False)
     cost = db.Column(db.Integer, unique=False, nullable=False)
-    Trip_in_website_feed = db.relationship("Feed", backref="Trip")
+    trip_in_match = db.relationship("MatchTrip")
 
     def serialize(self):
         return {
@@ -46,36 +46,22 @@ class Trip(db.Model):
         }
 
 
-class Feed(db.Model):
+class MatchTrip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    creator_of_the_trip = db.Column(db.Integer, db.ForeignKey("user.id"))
-    Owner_of_trip1 = db.relationship("User")
-    trip_of_an_user = db.Column(db.Integer, db.ForeignKey("trip.id"))
-    Owner_of_trip = db.relationship("Trip")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    trip_id = db.Column(db.Integer, db.ForeignKey("trip.id"))
+    user = db.relationship("User")
+    trip = db.relationship("Trip")
+    message = db.Column(db.Text, unique=False, nullable=False)
+    accepted = db.Column(db.Boolean, unique=False,
+                         nullable=True, default=False)
+    rejected = db.Column(db.Boolean, unique=False,
+                         nullable=True, default=False)
 
     def serialize(self):
         return {
             "id": self.id,
-        }
-
-
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_of_social_network = db.Column(db.Integer, db.ForeignKey("user.id"))
-    trip_of_social_network = db.Column(db.Integer, db.ForeignKey("trip.id"))
-    favorite_user_of_social_network = db.relationship("User")
-    favorite_trip_of_social_network = db.relationship("Trip")
-
-    def serialize(self):
-        return {
-            "id": self.id,
-        }
-
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    def serialize(self):
-        return {
-            "id": self.id,
+            "message": self.message,
+            "accepted": self.accepted,
+            "rejected": self.rejected,
         }
