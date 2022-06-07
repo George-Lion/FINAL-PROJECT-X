70ecfd0d4818bcb4c14d3c21ba69344ffd3f42d9
Jorge Leon <jorgeleonpicture@gmail.com>
2022-06-07 14:00:14 +0000

    start match

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -4,7 +4,7 @@ const getState = ({ getStore, getActions, setStore }) => {
       user: {},
       userProfiles: [],
       userTrips: [],
-      url: "https://3001-georgelion-finalproject-d16qehmb8rn.ws-eu47.gitpod.io/api/",
+      url: "https://3001-georgelion-finalproject-nah4r194rpj.ws-eu47.gitpod.io/api/",
       user_id: null,
       trips: [],
       logged: null,
@@ -56,7 +56,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           setStore({ user: data.user });
-        } catch (e) {}
+        } catch (e) { }
       },
 
       getUserTrips: async () => {
@@ -70,7 +70,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           setStore({ userTrips: data.trips });
-        } catch (e) {}
+        } catch (e) { }
       },
 
       getUserProfiles: async () => {
@@ -84,7 +84,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           setStore({ userProfiles: data.profiles });
-        } catch (e) {}
+        } catch (e) { }
       },
 
       editUser: async (user) => {
@@ -102,7 +102,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           setStore({ user: data.user });
-        } catch (e) {}
+        } catch (e) { }
       },
 
       createTrip: async (trip) => {
@@ -120,7 +120,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           getActions().getUserTrips();
-        } catch (e) {}
+        } catch (e) { }
       },
 
       getTrip: async (id) => {
@@ -161,7 +161,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           setStore({ trips: data.trips });
-        } catch (e) {}
+        } catch (e) { }
       },
 
       editTrip: async (trip) => {
@@ -182,7 +182,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
           const data = await resp.json();
           setStore({ trip: data.trip });
-        } catch (e) {}
+        } catch (e) { }
       },
       addToFavorite: async () => {
         const resp = await fetch(getStore().url + "tripLikes", {
